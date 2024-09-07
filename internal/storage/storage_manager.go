cb37e0e6a0cb75b1ea3024af919001f0a1733eb7
Paweł Farys <pmg.farys@gmail.com>
2024-09-07 16:32:40 +0200

    Indent fix

--- internal/storage/storage_manager.go
+++ internal/storage/storage_manager.go
@@ -11,74 +11,74 @@ import (
 )
 
 type Manager struct {
-    storages []Storage
+	storages []Storage
 }
 
 func NewStorageManager(config *config.StorageConfig) (*Manager, error) {
-    storages, err := NewStorage(config)
-    if err != nil {
-        return nil, err
-    }
-    return &Manager{storages: storages}, nil
+	storages, err := NewStorage(config)
+	if err != nil {
+		return nil, err
+	}
+	return &Manager{storages: storages}, nil
 }
 
 func (sm *Manager) SaveFile(data []byte) string {
-    t := time.Now()
-    fileName := fmt.Sprintf("vault-snapshot-%s.snap", t.Format("20060102150405"))
+	t := time.Now()
+	fileName := fmt.Sprintf("vault-snapshot-%s.snap", t.Format("20060102150405"))
 
-    var wg sync.WaitGroup
-    for _, storage := range sm.storages {
-        wg.Add(1)
-        go func(s Storage) {
-            defer wg.Done()
-            s.Write(fileName, data)
-        }(storage)
-    }
-    wg.Wait()
+	var wg sync.WaitGroup
+	for _, storage := range sm.storages {
+		wg.Add(1)
+		go func(s Storage) {
+			defer wg.Done()
+			s.Write(fileName, data)
+		}(storage)
+	}
+	wg.Wait()
 
-    return fileName
+	return fileName
 }
 
 func (sm *Manager) Cleanup(retention int) error {
-    var wg sync.WaitGroup
-    var mu sync.Mutex
-    var firstErr error
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	var firstErr error
 
-    for _, storage := range sm.storages {
-        wg.Add(1)
-        go func(s Storage) {
-            defer wg.Done()
-            files, err := s.List()
-            if err != nil {
-                log.Error("Cannot get files list from storage!", "storage", s.Name())
-                mu.Lock()
-                if firstErr == nil {
-                    firstErr = err
-                }
-                mu.Unlock()
-                return
-            }
+	for _, storage := range sm.storages {
+		wg.Add(1)
+		go func(s Storage) {
+			defer wg.Done()
+			files, err := s.List()
+			if err != nil {
+				log.Error("Cannot get files list from storage!", "storage", s.Name())
+				mu.Lock()
+				if firstErr == nil {
+					firstErr = err
+				}
+				mu.Unlock()
+				return
+			}
 
-            if len(files) <= retention {
-                log.Debug("There are no old snapshots to be removed", "storage", s.Name())
-                return
-            }
+			if len(files) <= retention {
+				log.Debug("There are no old snapshots to be removed", "storage", s.Name())
+				return
+			}
 
-            sort.Slice(files, func(i, j int) bool {
-                return files[i].ModTime.After(files[j].ModTime)
-            })
+			sort.Slice(files, func(i, j int) bool {
+				return files[i].ModTime.After(files[j].ModTime)
+			})
 
-            files = files[retention:]
+			files = files[retention:]
 
-            for _, file := range files {
-                err := s.Remove(file.Name)
-                if err != nil {
-                    log.Error("Failed to remove file from storage!", "storage", s.Name(), "file", file.Name)
-                }
-            }
-        }(storage)
-    }
-    wg.Wait()
+			for _, file := range files {
+				err := s.Remove(file.Name)
+				if err != nil {
+					log.Error("Failed to remove file from storage!", "storage", s.Name(), "file", file.Name)
+				}
+			}
+		}(storage)
+	}
+	wg.Wait()
 
-    return firstErr
+	return firstErr
 }
